13f6536613e1cdcad68bd0b8712bc8f4fd476353
Eris <eris@dragoneris.com>
2025-03-14 13:30:48 -0400

    Test commit

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM python:3.12
+# Start from a base image with python 3.11 preinstalled
+FROM buildpack-deps:bookworm
 
 RUN apt update
-RUN apt install -y wget libgl1 libglib2.0-0 python3-pip python3-venv
-RUN wget https://raw.githubusercontent.com/mcmonkeyprojects/SwarmUI/refs/heads/master/install-linux.sh
-COPY entrypoint.sh /entrypoint.sh
-RUN chmod +x install-linux.sh
-RUN chmod +x entrypoint.sh
+RUN apt install -y git libgl1 libglib2.0-0 python3-pip python3-venv
+RUN git clone https://github.com/derrian-distro/LoRA_Easy_Training_scripts_Backend
+WORKDIR /LoRA_Easy_Training_scripts_Backend
+RUN python3 installer.py colab
 
-EXPOSE 7801
+EXPOSE 8000
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-if [ -d "SwarmUI" ]; then
-	if [ -d "SwarmUI/dlbackend/ComfyUI/custom_nodes/ComfyUI-Manager" ]; then
-		./SwarmUI/launch-linux.sh --launch_mode none --host 0.0.0.0
-	else
-		# Install ComfyUI-Manager
-		git clone https://github.com/ltdrdata/ComfyUI-Manager/ "SwarmUI/dlbackend/ComfyUI/custom_nodes/ComfyUI-Manager"
-
-		# Grab autocomplete files
-		wget https://raw.githubusercontent.com/DominikDoom/a1111-sd-webui-tagcomplete/refs/heads/main/tags/danbooru.csv -o SwarmUI/Data/Autocompletions/danbooru.csv
-		wget https://raw.githubusercontent.com/DominikDoom/a1111-sd-webui-tagcomplete/refs/heads/main/tags/e621.csv -o SwarmUI/Data/Autocompletions/e621.csv
-		wget https://raw.githubusercontent.com/DominikDoom/a1111-sd-webui-tagcomplete/refs/heads/main/tags/danbooru_e621_merged.csv -o SwarmUI/Data/Autocompletions/danbooru_e621_merged.csv
-
-		./SwarmUI/launch-linux.sh --launch_mode none --host 0.0.0.0
-	fi
-else
-	./install-linux.sh --launch_mode none --host 0.0.0.0
-fi
+cd LoRA_Easy_Training_scripts_Backend
+venv/bin/python main.py
 
 exec "$@"
